cb87b25525cc182cd01c6131fef02064adfc8f24
Taush Sampley <taushsampley@gmail.com>
2025-11-23 14:34:18 -0600

    Add bare particle class

---
+++ scripts/physics/particle.js
@@ -0,0 +1,20 @@
+
+/**
+ * 
+ */
+class Particle {
+    constructor(x, y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    /**
+     * Progress the particle simulation by [delta].
+     * @param {number} delta 
+     */
+    step(delta) {
+        
+    }
+}
+
+export class Particle { }
