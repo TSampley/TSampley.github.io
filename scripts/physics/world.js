6bdfa80bf94b58a23652328adb8ba010efd844f1
Taush Sampley <taushsampley@gmail.com>
2025-11-23 14:35:03 -0600

    Add World type to manage world properties including date-time info

---
+++ scripts/physics/world.js
@@ -0,0 +1,62 @@
+
+
+import * as constants from './constants'
+
+/**
+ * The 
+ */
+class World {
+
+    constructor() {
+        this.milliseconds = 0;
+        this.seconds = 0;
+        this.minutes = 0;
+        this.hours = 0;
+        this.days = 0;
+        this.years = 0;
+    }
+
+    /**
+     * 
+     * @param {number} delta milliseconds elapsed in this frame
+     */
+    step(delta) {
+        if (!(delta instanceof Number)) {
+            console.error(`delta is not a Number: ${delta}`);
+            return;
+        }
+
+        this.milliseconds += delta;
+        if (this.milliseconds > constants.MILLIS_PER_SECOND) {
+            extraSeconds = this.milliseconds / constants.MILLIS_PER_SECOND;
+            this.milliseconds %= constants.MILLIS_PER_SECOND;
+
+            this.seconds += extraSeconds 
+            if (this.seconds > constants.SECONDS_PER_MINUTE) {
+                extraMinutes = this.seconds / constants.SECONDS_PER_MINUTE;
+                this.seconds %= constants.SECONDS_PER_MINUTE;
+
+                this.minutes += extraMinutes;
+                if (this.minutes > constants.MINUTES_PER_HOUR) {
+                    extraHours = this.minutes / constants.MINUTES_PER_HOUR;
+                    this.minutes %= constants.MINUTES_PER_HOUR;
+
+                    this.hours += extraHours;
+                    if (this.hours > constants.HOURS_PER_DAY) {
+                        extraDays = this.hours / constants.HOURS_PER_DAY;
+                        this.hours %= constants.HOURS_PER_DAY;
+
+                        if (this.days > constants.DAYS_PER_YEAR) {
+                            extraYears = this.days / constants.DAYS_PER_YEAR;
+                            this.days %= constants.DAYS_PER_YEAR;
+                            
+                            this.years += extraYears;
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
+export class World { }
