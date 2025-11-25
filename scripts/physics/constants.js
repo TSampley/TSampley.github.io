0a4b68106ae67b0246a45693c5454511165f0e15
Taush Sampley <taushsampley@gmail.com>
2025-11-25 05:53:50 -0600

    Move export statement to each definition

--- scripts/physics/constants.js
+++ scripts/physics/constants.js
@@ -4,34 +4,28 @@
 /**
  * https://en.wikipedia.org/wiki/Pi
  */
-const PI = 3.1415926535
+export const PI = 3.1415926535
 /**
  * https://en.wikipedia.org/wiki/E_(mathematical_constant)
  */
-const E = 2.71828
+export const E = 2.71828
 
 // #endregion
 
 // #region Time Constants
 
-const MILLIS_PER_SECOND = 1000;
-const SECONDS_PER_MINUTE = 60;
-const MINUTES_PER_HOUR = 60;
-const HOURS_PER_DAY = 24;
+export const MILLIS_PER_SECOND = 1000;
+export const SECONDS_PER_MINUTE = 60;
+export const MINUTES_PER_HOUR = 60;
+export const HOURS_PER_DAY = 24;
 
 /**
  * Milliseconds per day.
  * {@link MILLIS_PER_SECOND} * {@link SECONDS_PER_MINUTE} * 
  * {@link MINUTES_PER_HOUR} * {@link HOURS_PER_DAY}
  */
-const MILLIS_PER_DAY = 
+export const MILLIS_PER_DAY = 
     MILLIS_PER_SECOND * SECONDS_PER_MINUTE * 
     MINUTES_PER_HOUR * HOURS_PER_DAY;
 
 // #endregion
-
-export {
-    MILLIS_PER_DAY, 
-    MILLIS_PER_SECOND, SECONDS_PER_MINUTE, MINUTES_PER_HOUR, HOURS_PER_DAY,
-    E, PI
-};
