f1b1c56359af7a918f8193551e787408323de764
Taush Sampley <taushsampley@gmail.com>
2025-11-25 21:55:17 -0600

    Add quick play utility function to SoundBoard

--- scripts/physics/sound-board.mjs
+++ scripts/physics/sound-board.mjs
@@ -3,5 +3,12 @@ import { lazy } from '../common/lazy.mjs'
 
 export const SoundBoard = {
     clack: lazy(()=>{return new Audio('/assets/audio/billiard-ball-clack.wav')}),
-    pop: lazy(()=>{return new Audio('/assets/audio/mouth-pop-1.wav')})
+    pop: lazy(()=>{return new Audio('/assets/audio/mouth-pop-1.wav')}),
+
+    quickPlay: function (sound) {
+        const clip = sound.value
+        clip.pause()
+        clip.fastSeek(0)
+        clip.play()
+    }
 }
