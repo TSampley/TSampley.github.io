4deb22bd951802495b820f9f51ec14c405db3791
Taush Sampley <taushsampley@gmail.com>
2025-11-25 23:48:47 -0600

    Remove temp asset

--- scripts/audio/sound-board.mjs
+++ scripts/audio/sound-board.mjs
@@ -1,14 +1,26 @@
 
 import { lazy } from '../common/lazy.mjs'
 
-export const SoundBoard = {
-    clack: lazy(()=>{return new Audio('/assets/audio/billiard-ball-clack.wav')}),
-    pop: lazy(()=>{return new Audio('/assets/audio/mouth-pop-short.wav')}),
+const clack = lazy(()=>{return new Audio('/assets/audio/mouth-pop-short.wav')})
+const pop = lazy(()=>{return new Audio('/assets/audio/mouth-pop-short.wav')})
 
+export const SoundBoard = {
+
+    /**
+     * 
+     * @param {LazyValue<Audio>} sound 
+     */
     quickPlay: function (sound) {
         const clip = sound.value
         clip.pause()
         clip.fastSeek(0)
         clip.play()
+    },
+
+    playClack: function() {
+        this.quickPlay(clack)
+    },
+    playPop: function() {
+        this.quickPlay(pop)
     }
 }
