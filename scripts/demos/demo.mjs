cc281fa5055c501dee0c6fd578b1269305a0689b
Taush Sampley <taushsampley@gmail.com>
2025-11-25 18:32:31 -0600

    Add clack sound to play on mouse down events

--- scripts/demos/demo.mjs
+++ scripts/demos/demo.mjs
@@ -3,17 +3,28 @@
  * 
  */
 export class Demo {
+
+    #clackSound = new Audio('/assets/billiard-ball-clack.wav')
+
     /**
      * @param {String} id The HTMLElement id for the host canvas.
      */
     constructor(id) {
         this.canvas = document.getElementById(id);
         this.context = this.canvas.getContext('2d');
+
+        this.canvas.addEventListener('mousedown', (event)=> {
+            this.#clackSound.play();
+        });
     }
 
     addEventListener(event,listener) {
         this.canvas.addEventListener(event,listener);
     }
+    /**
+     * 
+     * @param {(MouseEvent)=>undefined} listener 
+     */
     addMouseDownListener(listener) {
         this.canvas.addEventListener('mousedown',listener);
     }
