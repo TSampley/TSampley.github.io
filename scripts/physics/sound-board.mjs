a82c649022c1b174b0fcea289b33d83058d5136e
Taush Sampley <taushsampley@gmail.com>
2025-11-25 23:40:02 -0600

    Replace mouth pop clip with cleaner clip

--- scripts/physics/sound-board.mjs
+++ scripts/physics/sound-board.mjs
@@ -3,7 +3,7 @@ import { lazy } from '../common/lazy.mjs'
 
 export const SoundBoard = {
     clack: lazy(()=>{return new Audio('/assets/audio/billiard-ball-clack.wav')}),
-    pop: lazy(()=>{return new Audio('/assets/audio/mouth-pop-1.wav')}),
+    pop: lazy(()=>{return new Audio('/assets/audio/mouth-pop-short.wav')}),
 
     quickPlay: function (sound) {
         const clip = sound.value
