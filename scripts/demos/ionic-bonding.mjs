9ef2fd15f464514e23b2c8c9a8dbfba7940b8aa4
Taush Sampley <taushsampley@gmail.com>
2025-11-25 18:32:59 -0600

    Fix invalid canvas references

--- scripts/demos/ionic-bonding.mjs
+++ scripts/demos/ionic-bonding.mjs
@@ -1,21 +1,30 @@
 
-import '../physics/world-controller.mjs';
-import '../physics/particle.mjs'
+import { WorldController } from '../physics/world-controller.mjs';
+import { Particle } from '../physics/particle.mjs'
 
-import '../demos/demo.mjs'
+import { Demo } from './demo.mjs'
 
-const demo = new Demo(id='hydrogen-bulk');
+const slider = document.getElementById('hydrogen-distance')
+
+const demo = new Demo('hydrogen-bulk');
 
 let controller = new WorldController()
 
+function setHydrogenDistance(value) {
+    alert(`Hydrogen Distance set to ${value}`)
+}
+
+slider.addEventListener('input',(event => {
+    setHydrogenDistance(event.target.value);
+}));
 demo.addMouseDownListener((event)=>{
     const x = event.x
     const y = event.y
     controller.addParticle(new Particle(x, y))
 })
 
-let width = canvas.width;
-let height = canvas.height;
+let width = demo.canvas.width;
+let height = demo.canvas.height;
 
 let x = 10;
 let y = 50;
@@ -50,7 +59,7 @@ function animate(timestamp) {
     let diff = timestamp - lastTime;
     step(diff);
 
-    context.clearRect(0, 0, canvas.width, canvas.height);
+    context.clearRect(0, 0, demo.canvas.width, demo.canvas.height);
 
     context.beginPath();
     context.ellipse(x, y, 10, 10, 0, 0, 2*Math.PI);
