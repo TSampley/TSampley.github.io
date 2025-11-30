e6226c2c483911a9a05523715b15427ed5635cd3
Taush Sampley <taushsampley@gmail.com>
2025-11-30 03:34:14 -0600

    Add vector page and script

---
+++ math/geometry/vector.md
@@ -0,0 +1,11 @@
+---
+layout: wiki
+
+title: Vector
+description:
+---
+
+
+
+<canvas class="demo-box" id='canvas-vector' width=500 height=500></canvas>
+
---
+++ math/geometry/vector.mjs
@@ -0,0 +1,89 @@
+
+import { UnimplementedError } from '../../scripts/common/errors.mjs'
+
+/**
+ * 
+ */
+export class Vector {
+    get magnitudeSqr() {
+        throw new UnimplementedError(this,'magnitudeSqr')
+    }
+    get magnitude() {
+        throw new UnimplementedError(this,'magnitude')
+    }
+    get direction() {
+        throw new UnimplementedError(this,'direction')
+    }
+}
+
+/**
+ * 
+ */
+export class Vector2D extends Vector {
+
+    /**
+     * 
+     * @param {number} x 
+     * @param {number} y 
+     */
+    constructor(x, y) {
+        this.x = x
+        this.y = y
+    }
+
+    get magnitudeSqr() {
+        return this.x*this.x + this.y*this.y
+    }
+
+    get magnitude() {
+        return Math.sqrt(this.magnitudeSqr)
+    }
+
+    get direction() {
+        return Math.atan2(this.y, this.x)
+    }
+
+    get slope() {
+        return this.y / this.x
+    }
+}
+
+/**
+ * The pair of angles in a spherical coordinate system.
+ * 
+ * https://en.wikipedia.org/wiki/Spherical_coordinate_system
+ */
+export class SphericalAngles {
+    /**
+     * 
+     * @param {number} theta Rise toward y-coordinate from run z-coordinate.
+     * @param {number} phi Rise toward y-coordinate from run x-coordinate.
+     */
+    constructor(theta,phi) {
+        this.theta = theta
+        this.phi = phi
+    }
+}
+
+/**
+ * 
+ */
+export class Vector3D extends Vector {
+    constructor(x,y,z) {
+        this.x = x
+        this.y = y
+        this.z = z
+    }
+    get magnitudeSqr() {
+        return this.x*this.x + this.y*this.y + this.z*this.z
+    }
+    get magnitude() {
+        return Math.sqrt(this.magnitudeSqr)
+    }
+    get direction() {
+        return new SphericalAngles(
+            Math.atan2(this.y, this.z),
+            Math.atan2(this.y, this.x)
+        )
+    }
+}
