fdb80f422845d62ffc56b0aa6eace5b4981c1177
Taush Sampley <taushsampley@gmail.com>
2025-11-30 20:20:09 -0600

    Add entity class

---
+++ scripts/physics/entity.mjs
@@ -0,0 +1,25 @@
+
+import { UnimplementedError } from '../common/errors.mjs'
+import { Environment } from '../Environment.mjs'
+
+/**
+ * A {@link Simulation} consists of an {@link Environment} and 
+ */
+export class Entity {
+    /**
+     * Progresses the entity by `dt` in the given `environment`.
+     * @param {number} dt Change in time for this simulation step.
+     * @param {Environment} environment 
+     */
+    step(dt,environment) {
+        throw new UnimplementedError(this,'step')
+    }
+
+    /**
+     * Draws the entity within the given rendering context.
+     * @param {CanvasRenderingContext2D} context 
+     */
+    draw(context) {
+        throw new UnimplementedError(this,'draw')
+    }
+}
