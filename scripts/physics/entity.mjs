bea14ce50915974d4ee214d6d1d612e3efac119d
Taush Sampley <taushsampley@gmail.com>
2025-11-30 23:54:40 -0600

    Add jsdoc to entity

--- scripts/physics/entity.mjs
+++ scripts/physics/entity.mjs
@@ -4,12 +4,13 @@ import { Environment } from '../physics/environment.mjs'
 
 /**
  * A {@link Simulation} consists of an {@link Environment} and 
+ * the `Entity` instances that populate it.
  */
 export class Entity {
     /**
      * Progresses the entity by `dt` in the given `environment`.
      * @param {number} dt Change in time for this simulation step.
-     * @param {Environment} environment 
+     * @param {Environment} environment The encompassing {@link Environment}
      */
     step(dt,environment) {
         throw new UnimplementedError(this,'step')
