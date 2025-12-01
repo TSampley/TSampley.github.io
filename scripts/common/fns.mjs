ac2e2b2223b21108bd64eea66fc6dc7f749797fd
Taush Sampley <taushsampley@gmail.com>
2025-11-30 20:17:38 -0600

    Add common no-op functions

---
+++ scripts/common/fns.mjs
@@ -0,0 +1,11 @@
+
+/**
+ * A collection of no-op functions, each referenced as `f#` where `#`
+ * is the number of parameters the function takes.
+ */
+export const NoOp = {
+    f0: ()=>{},
+    f1: (p1)=>{},
+    f2: (p1,p2)=>{},
+    f3: (p1,p2,p3)=>{}
+}
