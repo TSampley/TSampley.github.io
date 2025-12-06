a49f2016b89d5fb95214ed89128dee99de987005
Taush Sampley <taushsampley@gmail.com>
2025-12-06 03:56:16 -0600

    Add gravity units to jsdoc

--- science/physics/mechanics/gravity.mjs
+++ science/physics/mechanics/gravity.mjs
@@ -3,8 +3,14 @@ import { EnvironmentForce } from "./force.mjs"
 
 /**
  * TODO: move gravitational constant here?
+ * 
+ * $`F_g = G * m_a * m_b / d^2`$
  */
 export class Gravity extends EnvironmentForce {
+    /**
+     * 
+     * @param {number} value $`N * m^2 * kg^-2`$
+     */
     constructor(value) {
         super(value)
     }
