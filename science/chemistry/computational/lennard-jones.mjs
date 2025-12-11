999a50b1317308e58e23c73f15a342cf062d67e9
Taush Sampley <taushsampley@gmail.com>
2025-12-11 06:45:49 -0600

    limit lennard jones force by van der waals radius

--- science/chemistry/computational/lennard-jones.mjs
+++ science/chemistry/computational/lennard-jones.mjs
@@ -2,7 +2,7 @@
 import { InterparticleForce } from "../../physics/mechanics/force.mjs"
 
 
-const SOFT_CAP_SQR = 105E-12 ** 2
+const SOFT_CAP_SQR = 120E-12 ** 2
 /**
  * 
  * 
@@ -28,14 +28,14 @@ export class LennardJonesPotential extends InterparticleForce {
         // TODO: explore soft-core potentials
         //   https://pmc.ncbi.nlm.nih.gov/articles/PMC3187911/;
         //   https://www.sciencedirect.com/science/article/abs/pii/S1093326303001967 
-        const deltaSqr = Math.max(deltaX*deltaX + deltaY*deltaY, SOFT_CAP_SQR)
+        const sigma = (alpha.props.atomicRadius + beta.props.atomicRadius) // PM
+        const deltaSqr = Math.max(deltaX*deltaX + deltaY*deltaY, sigma*sigma)
         const deltaMag = Math.sqrt(deltaSqr)
 
         // Lennard-Jones Potential
         // epsilon: depth of potential well // TODO: determine depth of well
         const epsilon = 1E-12 // eV
         // sigma: finite distance where potential is zero, i.e., atoms "collide"
-        const sigma = (alpha.props.atomicRadius + beta.props.atomicRadius) // PM
         const sigmaOverDistance = sigma / deltaMag
         console.log(`${sigma} / ${deltaMag} = ${sigmaOverDistance}`)
         const s6 = sigmaOverDistance**6
