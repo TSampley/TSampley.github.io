b49c0d30d4829c603263368754e6c473722ef070
Taush Sampley <taushsampley@gmail.com>
2025-12-12 05:11:38 -0600

    Adjust lennard jones force for accuracy

--- science/chemistry/computational/lennard-jones.mjs
+++ science/chemistry/computational/lennard-jones.mjs
@@ -3,6 +3,10 @@ import { InterparticleForce } from "../../physics/mechanics/force.mjs"
 
 
 const SOFT_CAP_SQR = 120E-12 ** 2
+const JOULES_PER_EV = 1.602176634E-19
+const MOLE = 6.022E23 // particles/mol
+const MOLES_PER_PARTICLE = 1 / MOLE // mol/particle
+
 /**
  * 
  * 
@@ -34,16 +38,17 @@ export class LennardJonesPotential extends InterparticleForce {
 
         // Lennard-Jones Potential
         // epsilon: depth of potential well // TODO: determine depth of well
-        const epsilon = 1E-12 // eV
+        const epsilon = -500 // J / mol
+        const energyPerParticle = epsilon // * MOLES_PER_PARTICLE
         // sigma: finite distance where potential is zero, i.e., atoms "collide"
         const sigmaOverDistance = sigma / deltaMag
         console.log(`${sigma} / ${deltaMag} = ${sigmaOverDistance}`)
         const s6 = sigmaOverDistance**6
         const s12 = s6*s6
-        const total = 24 * epsilon / deltaMag * (2*s12 - s6)
+        const total = 24 * energyPerParticle / deltaMag * (2*s12 - s6)
         console.log(`lennard jones: ${total}`)
-        const forceX = - total * deltaX / deltaMag
-        const forceY = - total * deltaY / deltaMag
+        const forceX = total * deltaX / deltaMag
+        const forceY = total * deltaY / deltaMag
 
         // TODO: convert eV to newtons or fxy always means eV for atoms?
         alpha.fx += forceX
