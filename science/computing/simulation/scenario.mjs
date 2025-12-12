206fea9643dc5faf4311fad034599dfb8971370a
Taush Sampley <taushsampley@gmail.com>
2025-12-12 05:09:45 -0600

    Create new scenario type

---
+++ science/computing/simulation/scenario.mjs
@@ -0,0 +1,30 @@
+import { ForceMatrix } from "./environment.mjs"
+import { Particle } from "../../physics/mechanics/particle.mjs"
+
+/**
+ * Generic container class for a configurable simulation
+ * scenario, including simulated entities, and environment
+ * state.
+ */
+export class Scenario {
+
+    /**
+     * 
+     * @param {string} name
+     * @param {ForceMatrix}
+     * @param {()=>Array<Particle>} spawner 
+     */
+    constructor(name,forceMatrix,spawner) {
+        this.name=name
+        this.forceMatrix=forceMatrix
+        this.spawner=spawner
+    }
+
+    /**
+     * 
+     * @returns {Array<Particle>}
+     */
+    spawnParticles() {
+        return this.spawner()
+    }
+}
