a65c9bfbbb60b419539be376817336a2da66ff0a
Taush Sampley <taushsampley@gmail.com>
2025-12-26 09:54:43 -0600

    Replace fastSeek with seek in quickPlay method

--- scripts/audio/sound-board.mjs
+++ scripts/audio/sound-board.mjs
@@ -14,7 +14,7 @@ export const SoundBoard = {
     quickPlay: function (sound) {
         const clip = sound.value
         clip.pause()
-        clip.fastSeek(0)
+        clip.seek(0)
         clip.play()
     },
 
