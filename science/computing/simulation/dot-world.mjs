7500105d2962aa962bd879d94fa6bab39b1e35b6
Taush Sampley <taushsampley@gmail.com>
2026-01-08 08:14:22 -0600

    Rename ball world to dot world

---
+++ science/computing/simulation/dot-world.mjs
@@ -0,0 +1,18 @@
+import { BaseEnvironment } from "./environment.mjs";
+
+/**
+ * A world full of dots, interacting in social (or ecological) simulations.
+ */
+export class DotWorld extends BaseEnvironment {
+
+  /**
+   * @param {number} width
+   * @param {number} height
+   */
+  constructor(width,height) {
+    super(width,height)
+
+  }
+
+  
+}
