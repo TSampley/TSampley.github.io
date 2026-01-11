3fa2dc7742f0cf40290540839bbfdb1a70e593e7
Taush Sampley <taushsampley@gmail.com>
2026-01-11 00:21:42 -0600

    Add Dot World module

---
+++ science/sociology/dot-world.mjs
@@ -0,0 +1,40 @@
+import { BaseEnvironment } from "../computing/simulation/environment.mjs";
+
+/**
+ * A world full of dots, interacting in social (or ecological) simulations.
+ */
+export class DotWorld extends BaseEnvironment {
+
+  /**
+   * @param {number} width
+   * @param {number} height
+   */
+  constructor(width,height) {
+    super(width,height)
+
+  }
+
+  step(delta) {
+    // determine day/night cycle
+
+    // determine weather conditions
+
+    // determine energy contributed to environment
+
+    // 
+  }
+
+  draw(context,offset) {
+    // clear area
+
+  }
+}
+// TODO: create aggression page for simulation demo
+    /*
+    draw entities in z-order
+    - food
+    - plants
+    - animals
+      - bunny dot 
+      - wolf dot
+    */
