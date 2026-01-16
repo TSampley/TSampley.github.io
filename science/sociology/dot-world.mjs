7ddc06996e3612092119618525505cbe779d10ef
Taush Sampley <taushsampley@gmail.com>
2026-01-16 02:25:04 -0600

    Add dot world controller for demo

--- science/sociology/dot-world.mjs
+++ science/sociology/dot-world.mjs
@@ -1,40 +1,140 @@
-import { BaseEnvironment } from "../computing/simulation/environment.mjs";
+import { Size } from "../../js/common/geom.mjs";
+import { Simulation } from "../computing/simulation/simulation.mjs";
+import { Environment } from "../computing/simulation/environment.mjs";
+
+
+/**
+ * Seconds per day
+ */
+const DAY_PERIOD = 1000
+/**
+ * Days per year
+ */
+const YEAR_PERIOD = 100
+
+const EARTH_TILT = 23.5 * Math.PI / 180
+const OK_LAT = 35
 
 /**
  * A world full of dots, interacting in social (or ecological) simulations.
+ * 
+ * TODO: plant/heat dynamics
+ * - once plants reach size/cycle, produce flowers
+ * - flowers produce seeds that disperse on wind
+ * - wind determined by air temperatures driving convection
  */
 export class DotWorld extends Environment {
 
   /**
-   * @param {number} width
-   * @param {number} height
+   * @param {Size} size
    */
-  constructor(width,height) {
-    super(width,height)
+  constructor(size, axisOffset = EARTH_TILT, latitude = OK_LAT) {
+    super(size)
 
+    this.plants = []
+    this.bunnies = []
+    this.wolves = []
+
+    this.time = 0
+
+    this.axisOffset = axisOffset
+    this.latitude = latitude
+
+    this.dayPhase = 0
+    this.annualPhase = 0
   }
 
   step(delta) {
-    // determine day/night cycle
+    // progress total elapsed time
+    this.time += delta
+    // determine season + day/night cycle
+    const seconds = this.time % DAY_PERIOD
+    const dayPhase = seconds / DAY_PERIOD
+    const days = Math.floor(this.time / DAY_PERIOD)
+    /** [0, 1) */
+    const annualPhase = days % YEAR_PERIOD
+    /** [0, inf) */
+    const year = Math.floor(days / YEAR_PERIOD)
+    const dayOfYear = days % YEAR_PERIOD
+    /** [0, 2Pi) */
+    const seasonPhase = Math.PI * 2 * annualPhase
 
     // determine weather conditions
+    const transmission = 1 // always sunny for now
 
-    // determine energy contributed to environment
+    // determine energy contributed across environment
+    const energyFlux = 10 // J * m^-2 * s^-1
 
-    // 
+    // plants grow, reproduce, die
+    for (const plant of this.plants) {
+      const surfaceArea = plant.size || Math.PI*100
+      const incidentEnergy = surfaceArea * energyFlux
+
+      plant.absorb(incidentEnergy)
+    }
+
+    // bunnies forage, reproduce, die
+    for (const bunny of this.bunnies) {
+
+    }
+
+    // wolves hunt, reproduce, die
+    for (const wolf of this.wolves) {
+
+    }
   }
 
-  draw(context,offset) {
+  /**
+   * 
+   * @param {CanvasRenderingContext2D} context 
+   * @param {number} offset 
+   */
+  draw(context, offset) {
     // clear area
+    context.clearRect(this.width, this.height)
+    context.save()
 
+    // set color filter
+    const brightness = Math.max(0, Math.cos(this.dayPhase / 2))
+    context.filter = `brightness(${brightness}%)`
+
+    // draw field
+
+    // draw plants
+
+    // draw bunnies
+
+    // draw wolves
+
+    context.restore()
   }
 }
-// TODO: create aggression page for simulation demo
-    /*
-    draw entities in z-order
-    - food
-    - plants
-    - animals
-      - bunny dot 
-      - wolf dot
-    */
+
+/**
+ * 
+ */
+export class DotWorldController {
+  /**
+   * 
+   * @param {*} canvasId 
+   */
+  constructor(canvasId) {
+    /** @type {HTMLCanvasElement} */
+    this.canvas = document.getElementById(canvasId)
+    /** @type {CanvasRenderingContext2D} */
+    this.context = this.canvas.getContext('2d')
+
+    const size = new Size(this.canvas.width, this.canvas.height)
+    this.dotWorld = new DotWorld(size)
+    this.simulation = new Simulation(this.dotWorld, this.context)
+  }
+
+  onStart() {
+    this.simulation.start()
+  }
+
+  onStop() {
+    this.simulation.stop()
+  }
+
+}
--- science/sociology/index.md
+++ science/sociology/index.md
@@ -16,7 +16,16 @@ When turning our attention inward, we can attempt to overcome our self-congratul
 - {: #sociological-perspective } Sociological Perspective - a perspective we take when studying cultures that attempts to remain impartial.
 {: .definitions}
 
+We'll try to keep things simple and fun as long as we can – before we get to the dark stuff – with our little dot friends in their Dot World.
 
+<canvas id='dot-canvas'>Your Browser does not support canvas.</canvas>
+<script type='module'>
+  import { DotWorldController } from './dot-world.mjs'
+  const controller = new DotWorldController('dot-canvas')
+  controller.onStart()
+</script>
+
+<!-- Notes to Organize
 - https://readingdecoloniality.warwick.ac.uk/this-is-the-oppressors-language-yet-i-need-it-to-talk-to-you/
 - https://en.wikipedia.org/wiki/Pedagogy_of_the_Oppressed
-
+ -->
