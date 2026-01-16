8ea54565d085b3b2bd2b3b7e2c3bf1d125a81ec1
Taush Sampley <taushsampley@gmail.com>
2026-01-16 02:46:15 -0600

    Refactor Simulation class to include context parameter and improve step handling

--- science/computing/simulation/simulation.mjs
+++ science/computing/simulation/simulation.mjs
@@ -1,14 +1,13 @@
-
-
-import { Environment } from './environment.mjs';
-
 import { Timer } from '../../../js/common/timer.mjs'
 import { Entity } from './entity.mjs';
+import { Environment } from './environment.mjs';
 
 /**
  * 
  * A `Simulation` is responsible for the time-keeping
  * mechanisms in order to simulate an {@link Environment}.
+ * It also retains a reference to a rendering context to
+ * pass to the environment at the appropriate time.
  * 
  * @type {E extends Environment}
  */
@@ -17,8 +16,9 @@ export class Simulation {
     /**
      * 
      * @param {Environment} environment 
+     * @param {CanvasRenderingContext2D} context
      */
-    constructor(environment) {
+    constructor(environment,context) {
         this.world = new Timer()
         /**
          * @type {Array<Particle>}
@@ -29,15 +29,33 @@ export class Simulation {
          */
         this.entityList = new Array()
         this.environment = environment
+        this.context = context
     }
 
     #isRunning = false
     start() {
+        if (this.#isRunning) return;
         this.#isRunning = true
-        requestAnimationFrame(this.#step)
+        this.#step()
     }
 
-    #step(delta) {
+    #step() {
+        requestAnimationFrame((tsl)=>{this.#frame(tsl)})
+    }
 
+    #lastTime = 0
+    #frame(tsl) {
+        if (!this.#lastTime) this.#lastTime = tsl
+        const delta = (tsl - this.#lastTime) / 1000.0
+
+        this.world.step(delta)
+        this.environment.step(delta)
+        this.environment.draw(this.context,0)
+
+        if (this.#isRunning) this.#step()
+    }
+
+    stop() {
+        this.#isRunning = false
     }
 }
