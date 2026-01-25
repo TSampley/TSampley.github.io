d11d9678f233dfaf0bd8ce5c16be71a361ec96c4
Taush Sampley <taushsampley@gmail.com>
2026-01-24 22:52:12 -0600

    Add canvas size updates when resized

--- science/computing/graphics/svg-editor.md
+++ science/computing/graphics/svg-editor.md
@@ -6,7 +6,7 @@ module: ./svg-editor.mjs
 
 <div id='app-host' class='container'>
   <noscript>Please Enable Javascript to use the applet</noscript>
-  <canvas id='canvas' class='demo-canvas'>Your browser does not support canvas.</canvas>
+  <canvas id='canvas' class='demo-canvas' style='width: 100%; height: 100%;'>Your browser does not support canvas.</canvas>
   <div id='controls' class=''>
     <div id='toolbar'></div>
   </div>
--- science/computing/graphics/svg-editor.mjs
+++ science/computing/graphics/svg-editor.mjs
@@ -13,11 +13,26 @@ export class Component {
  */
 export class SvgEditor extends Component {
   constructor() {
+    super()
     this.canvas = document.getElementById('canvas')
     this.context = this.canvas.getContext('2d')
 
     this.toolbar = document.getElementById('toolbar')
   }
+
+  resizeCanvas() {
+    const width = this.canvas.clientWidth
+    const height = this.canvas.clientHeight
+
+    if (width != this.canvas.width || height != this.canvas.height) {
+      console.info('SvgEditor.resizeCanvas')
+      this.canvas.width = width
+      this.canvas.height = height
+    }
+  }
 }
 
 const editor = new SvgEditor()
+const observer = new ResizeObserver(()=>editor.resizeCanvas())
+observer.observe(editor.canvas)
+editor.resizeCanvas()
