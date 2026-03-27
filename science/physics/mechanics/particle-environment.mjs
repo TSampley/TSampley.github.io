a3170909d22ec410388316ecd96f5f2b93193c65
Taush Sampley <taushsampley@gmail.com>
2026-03-27 02:51:10 -0500

    Move particle simulation logic to environment

--- science/physics/mechanics/particle-environment.mjs
+++ science/physics/mechanics/particle-environment.mjs
@@ -1,7 +1,8 @@
-import { Size } from "../../../js/common/geom.mjs";
-import { Observable, single } from "../../../js/common/observables.mjs";
-import { ForceMatrix } from "../../chemistry/computational/force-matrix.mjs";
-import { Environment } from "../../computing/simulation/environment.mjs";
+import { Size } from "/js/common/geom.mjs";
+import { NoOp } from "/js/common/fns.mjs";
+import { Observable, single } from "/js/common/observables.mjs";
+import { ForceMatrix } from "/chemistry/computational/force-matrix.mjs";
+import { Environment } from "/computing/simulation/environment.mjs";
 
 /**
  */
@@ -18,10 +19,148 @@ export class ParticleEnvironment extends Environment {
     console.log("Property" + JSON.stringify(this.forceMatrix.value))
     this.timeScale = 1E-6
     this.hardCollisions = true
+
+    /** @type {Array<Particle>} */
+    this.particleList = new Array()
+
+    /** @type {()=>void} */
+    this.onBounce = NoOp.f0
+    /** @type {()=>void} */
+    this.onCollision = NoOp.f0
   }
   
+  /** @param {number} delta */
   step(delta) {
+    const step = delta * this.timeScale
+    // Clear previous state
+    for (const particle of this.particleList) {
+      particle.clearForces()
+    }
 
+    // Calculate all forces and collisions
+    const particleForces = this.particleForces().filter((value) => {
+      return value.isEnabled
+    })
+    const environmentForces = this.environmentForces().filter((value) => {
+      return value.isEnabled
+    })
+    const max = this.particleList.length
+    for (let alphaIndex = 0; alphaIndex < max; alphaIndex++) {
+      const alpha = this.particleList[alphaIndex]
+      for (let betaIndex = alphaIndex + 1; betaIndex < max; betaIndex++) {
+        const beta = this.particleList[betaIndex]
+        particleForces.forEach((force) => {
+          force.applyForce(step, alpha, beta)
+        })
+      }
+      environmentForces.forEach((force) => {
+        force.applyForce(step, alpha)
+      })
+      // TODO: check accumulated force here
+    }
+
+    // Integrate entities
+    for (const particle of this.particleList) {
+      particle.integrate(step)
+    }
+
+    // Resolve collisions
+    for (let index = 0; index < max; index++) {
+      const alpha = this.particleList[index]
+      if (this.hardCollisions) {
+        for (let otherIndex = index + 1; otherIndex < max; otherIndex++) {
+          const beta = this.particleList[otherIndex]
+          this.checkParticleCollision(alpha, beta, )
+        }
+      }
+      this.checkEnvironmentCollision(alpha, )
+    }
+  }
+
+  /**
+   * @param {Particle} subject
+   */
+  checkEnvironmentCollision(subject) {
+    if (subject.x > this.width) {
+      this.onBounce();
+      subject.vx *= -this.forceMatrix.value.boundaries.value;
+      subject.x = 2 * this.width - subject.x;
+    } else if (subject.x < 0) {
+      this.onBounce();
+      subject.vx *= -this.forceMatrix.value.boundaries.value;
+      subject.x = -subject.x;
+    }
+
+    if (subject.y > this.height) {
+      this.onBounce();
+      subject.vy *= -this.forceMatrix.value.boundaries.value;
+      subject.y = 2 * this.height - subject.y;
+    } else if (subject.y < 0) {
+      this.onBounce();
+      subject.vy *= -this.forceMatrix.value.boundaries.value;
+      subject.y = -subject.y;
+    }
+  }
+
+  /**
+   * Checks the two particles against each other for collision and resolves
+   * it if detected, applying appropriate forces to each particle.
+   * 
+   * @param {Particle} alpha The first particle to check for collision. 
+   * @param {Particle} beta The other particle to check for collision. 
+   */
+  checkParticleCollision(alpha, beta) {
+    // Calculate vector between particles
+    const deltaX = beta.x - alpha.x
+    const deltaY = beta.y - alpha.y
+    const deltaSqr = deltaX * deltaX + deltaY * deltaY;
+
+    // Calculate Properties
+    const radiusSum = alpha.props.atomicRadius + beta.props.atomicRadius
+    const radiusSumSqr = radiusSum * radiusSum
+    const alphaMass = alpha.props.mass
+    const betaMass = beta.props.mass
+    const totalMass = alphaMass + betaMass
+    const massDiff = alphaMass - betaMass
+
+    // Ensure particles within collision range
+    if (deltaSqr <= radiusSumSqr) {
+      const velX = beta.vx - alpha.vx
+      const velY = beta.vy - alpha.vy
+      const dotProd = velX * deltaX + velY * deltaY
+      // ensure velocities are opposed before reversing
+      if (dotProd < 0) {
+        // Resolve Collision
+
+        // decompose velocity into parallel and opposing components
+        const alphaDiffMag = (alpha.vx * deltaX + alpha.vy * deltaY) / deltaSqr
+        const alphaTanMag = (alpha.vx * deltaY - alpha.vy * deltaX) / deltaSqr
+        const betaDiffMag = (beta.vx * deltaX + beta.vy * deltaY) / deltaSqr
+        const betaTanMag = (beta.vx * deltaY - beta.vy * deltaX) / deltaSqr
+
+        // calculate new components along collision path
+        const finalAlphaDiffMag = (massDiff / totalMass) * alphaDiffMag + 2 * betaMass / totalMass * betaDiffMag
+        const finalBetaDiffMag = 2 * alphaMass / totalMass * alphaDiffMag - (massDiff / totalMass) * betaDiffMag
+
+        // recompose new velocities
+        alpha.vx = finalAlphaDiffMag * deltaX + alphaTanMag * deltaY
+        alpha.vy = finalAlphaDiffMag * deltaY + alphaTanMag * (-deltaX)
+        beta.vx = finalBetaDiffMag * deltaX + betaTanMag * deltaY
+        beta.vy = finalBetaDiffMag * deltaY + betaTanMag * (-deltaX)
+
+        this.onCollide()
+      }
+
+      // nudge particles unconditionally
+      const deltaMag = Math.sqrt(deltaSqr)
+      const diff = radiusSum - deltaMag
+      const nudgeX = diff * deltaX / (deltaMag * totalMass);
+      const nudgeY = diff * deltaY / (deltaMag * totalMass);
+      alpha.x -= nudgeX * betaMass;
+      alpha.y -= nudgeY * betaMass;
+      beta.x += nudgeX * alphaMass;
+      beta.y += nudgeY * alphaMass;
+    }
   }
 
   draw(context,offset) {
