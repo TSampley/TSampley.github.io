16e60f3fff5223954b086b7a65d94b3b461fa212
Taush Sampley <taushsampley@gmail.com>
2026-03-27 03:54:07 -0500

    Copy BaseSimulation code for phase offset

--- science/computing/simulation/simulation.mjs
+++ science/computing/simulation/simulation.mjs
@@ -84,3 +84,58 @@ export class Simulation {
         this.#isRunning = false
     }
 }
+
+// TODO: reference below to convert to integrate phase offset parameter
+    // /**
+    //  * 
+    //  * @param {Environment} subject 
+    //  */
+    // constructor(subject) {
+    //     this.subject = subject
+
+    //     /** @type {Observable<boolean>} */
+    //     this.isRunning = single(true)
+    //     /** @type {Observable<number>} */
+    //     this.simulationDelta = single(1000 / 60.0)
+    //     /** @type {Observable<number>} */
+    //     this.animationDelta = single(1000 / 30.0)
+    // }
+
+    // start() {
+    //     this.isRunning.value = true
+
+    //     setTimeout()
+    //     // TODO: consider 
+    //     requestAnimationFrame((tls)=>this.#animate(tls))
+    // }
+
+    // #lastTime = 0
+    // #simulationPhase = 0
+    // #animationPhase = 0
+    // #animate(tsl) {
+    //     if (this.#lastTime == 0) {
+    //         this.#lastTime = tsl
+    //     }
+
+    //     const deltaMs = tsl - this.#lastTime
+
+    //     while (this.#simulationPhase >= this.simulationDelta) {
+    //         this.step(this.simulationDelta)
+    //     }
+
+    //     const offset = (this.#animationPhase + deltaMs) % this.animationDelta
+    //     this.#animationPhase = phase
+    //     this.draw(phase)
+    // }
+
+    // stop() {
+    //     this.isRunning.value = false
+    // }
+
+    // step(delta) {
+    //     this.subject.step(delta)
+    // }
+
+    // draw() {
+    //     this.subject.draw(context,0)
+    // }
