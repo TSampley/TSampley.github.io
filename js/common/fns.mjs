7e211bf6ea4d69424f81740e02fa7aabb7876200
Taush Sampley <taushsampley@gmail.com>
2026-04-01 01:00:23 -0500

    Move sleep promise function to fns file

--- js/common/fns.mjs
+++ js/common/fns.mjs
@@ -9,3 +9,10 @@ export const NoOp = {
     f2: (p1,p2)=>{},
     f3: (p1,p2,p3)=>{}
 }
+
+/**
+ * Sleeps for the given number of milliseconds.
+ * @param {number} delay milliseconds
+ * @returns A new Promise that resolves after the given delay.
+ */
+export const sleep = (delay) => new Promise((resolve) => setTimeout(resolve, delay))
