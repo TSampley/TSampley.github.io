a6c0ab5427754651323af7b0eaccffac12fae159
Taush Sampley <taushsampley@gmail.com>
2026-04-01 01:14:03 -0500

    Add geom jsdoc

--- js/common/geom.mjs
+++ js/common/geom.mjs
@@ -1,4 +1,14 @@
+/*
+ * Copyright (c) 2026 Taush Sampley
+ * This software is released under the GPL-3.0 License.
+ * See LICENSE file in the project root for full license information.
+ * 
+ * geom.mjs - geometric primitives
+ */
 
+/**
+ * A pair of numbers representing a width and height.
+ */
 export class Size {
   constructor(width,height) {
     this.width = width
@@ -6,6 +16,9 @@ export class Size {
   }
 }
 
+/**
+ * A pair of numbers representing a point in 2D space, x, y.
+ */
 export class Point {
   constructor(x=0,y=0) {
     this.x = x
