9ec4c54a10091b8b1fccbb437e856f874bc65fde
Taush Sampley <taushsampley@gmail.com>
2026-03-31 21:38:13 -0500

    Initialize environment for dot-product demo

--- math/linear-algebra/dot-products.mjs
+++ math/linear-algebra/dot-products.mjs
@@ -1,13 +1,20 @@
-import { Demo } from "../../js/components/demo.mjs";
-import { TimeControls } from "../../js/components/time-controls.mjs"
-import { Simulation } from "../../science/computing/simulation/simulation.mjs";
+import { Size } from "/js/common/geom.mjs";
+import { Demo } from "/js/components/demo.mjs";
+import { TimeControls } from "/js/components/time-controls.mjs"
+import { Environment } from "/science/computing/simulation/environment.mjs";
+import { Simulation } from "/science/computing/simulation/simulation.mjs";
 
 // Collect UI elements
 const demo = new Demo('canvas-dot-product')
 const controls = new TimeControls('button-start','button-stop')
 
+const environment = new Environment(new Size(demo.canvas.width, demo.canvas.height))
+const scenarios = []
 // Initialize Model
-const simulation = new Simulation()
+const simulation = new Simulation(environment, scenarios, (enviro,offset)=> {
+  demo.context.clearRect(0, 0, demo.canvas.width, demo.canvas.height)
+  // TODO: render the environment
+})
 
 // Setup Event Listeners
 // == demo.canvas mouseListeners
