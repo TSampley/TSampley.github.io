2f350dab1776fcb7a30141e37cffbcb0a6f0e616
Taush Sampley <taushsampley@gmail.com>
2026-04-08 21:43:15 -0500

    Fix undeclared variable

--- js/common/timer.mjs
+++ js/common/timer.mjs
@@ -1,5 +1,11 @@
 
 
+const constants = {
+    SECONDS_PER_MINUTE: 60,
+    MINUTES_PER_HOUR: 60,
+    HOURS_PER_DAY: 24,
+    DAYS_PER_YEAR: 365
+}
 
 /**
  * The 
@@ -26,21 +32,22 @@ export class Timer {
 
         this.seconds += delta
         if (this.seconds > constants.SECONDS_PER_MINUTE) {
-            extraMinutes = this.seconds / constants.SECONDS_PER_MINUTE;
+            const extraMinutes = this.seconds / constants.SECONDS_PER_MINUTE;
             this.seconds %= constants.SECONDS_PER_MINUTE;
 
             this.minutes += extraMinutes;
             if (this.minutes > constants.MINUTES_PER_HOUR) {
-                extraHours = this.minutes / constants.MINUTES_PER_HOUR;
+                const extraHours = this.minutes / constants.MINUTES_PER_HOUR;
                 this.minutes %= constants.MINUTES_PER_HOUR;
 
                 this.hours += extraHours;
                 if (this.hours > constants.HOURS_PER_DAY) {
-                    extraDays = this.hours / constants.HOURS_PER_DAY;
+                    const extraDays = this.hours / constants.HOURS_PER_DAY;
                     this.hours %= constants.HOURS_PER_DAY;
 
+                    this.days += extraDays;
                     if (this.days > constants.DAYS_PER_YEAR) {
-                        extraYears = this.days / constants.DAYS_PER_YEAR;
+                        const extraYears = this.days / constants.DAYS_PER_YEAR;
                         this.days %= constants.DAYS_PER_YEAR;
                         
                         this.years += extraYears;
