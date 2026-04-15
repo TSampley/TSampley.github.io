6a2b1a3f2e03e4322d061160881006860fae8476
Taush Sampley <taushsampley@gmail.com>
2026-04-14 23:14:36 -0500

    Enforce 2 space indentation

--- eslint.config.js
+++ eslint.config.js
@@ -3,11 +3,18 @@ import jest from "eslint-plugin-jest";
 import globals from "globals";
 import tseslint from "typescript-eslint";
 import { defineConfig,globalIgnores } from "eslint/config";
+import stylistic from '@stylistic/eslint-plugin'
 
 export default defineConfig([
   { 
     files: ["**/*.{js,mjs,cjs,ts,mts,cts}"], 
-    plugins: { js }, 
+    plugins: { 
+      js,
+      '@stylistic': stylistic
+    },
+    rules: {
+      "@stylistic/indent": ['error', 2]
+    },
     extends: ["js/recommended"], 
     languageOptions: { 
       globals: globals.browser,
