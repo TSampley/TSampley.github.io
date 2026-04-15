07127e8e386751e38dab2593a91cbafd2bbabd71
Taush Sampley <taushsampley@gmail.com>
2026-04-15 00:06:54 -0500

    Delint force.mjs

--- science/physics/mechanics/force.mjs
+++ science/physics/mechanics/force.mjs
@@ -1,17 +1,21 @@
 
-import { Particle } from '/science/physics/mechanics/particle.mjs'
 
 /**
- * 
+ * @typedef { import('/science/physics/mechanics/particle.mjs').Particle } Particle
+ */
+
+/**
+ * A generic force with scalar property that can be disabled.
  */
 export class Force {
   /**
    * 
    * @param {number} value 
+   * @param {boolean} isEnabled 
    */
-  constructor(value) {
+  constructor(value,isEnabled=true) {
     this.value = value
-    this.isEnabled = true
+    this.isEnabled = isEnabled
   }
 }
 
@@ -25,6 +29,7 @@ export class EnvironmentForce extends Force {
    * @param {number} dt The span of time to simulate over.
    * @param {Particle} subject The particle under influence of the force.
    */
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   applyForce(dt,subject) {}
 }
 
@@ -33,5 +38,12 @@ export class EnvironmentForce extends Force {
  */
 export class InterparticleForce extends Force {
   constructor(value) { super(value) }
+  /**
+   * 
+   * @param {number} dt The span of time to simulate over.
+   * @param {Particle} alpha The first particle under influence of the other.
+   * @param {Particle} beta The second particle under influence of the first.
+   */
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   applyForce(dt,alpha,beta) {}
 }
